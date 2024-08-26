dbe18eb4ebe27d58280be98b612ab6dd71ac1587
Jatoscano <jatoscano@uce.edu.ec>
2024-08-26 11:45:23 -0500

    Cammbios de Seleccion

--- app/src/main/java/com/gualoto/pfinaldm/ui/adapters/perfil/ViewPagerAdapter2.kt
+++ app/src/main/java/com/gualoto/pfinaldm/ui/adapters/perfil/ViewPagerAdapter2.kt
@@ -3,8 +3,6 @@ package com.gualoto.pfinaldm.ui.adapters.perfil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
-import com.gualoto.pfinaldm.ui.fragments.main.perfil.FavoritosFragment
-import com.gualoto.pfinaldm.ui.fragments.main.perfil.RecientesFragment
 
 class ViewPagerAdapter2(fragmentActivity: FragmentActivity) : FragmentStateAdapter(fragmentActivity) {
     override fun getItemCount(): Int {
@@ -13,8 +11,7 @@ class ViewPagerAdapter2(fragmentActivity: FragmentActivity) : FragmentStateAdapt
 
     override fun createFragment(position: Int): Fragment {
         return when (position) {
-            0 -> FavoritosFragment()
-            1 -> RecientesFragment()
+
             else -> throw IllegalStateException("Unexpected position $position")
         }
     }
